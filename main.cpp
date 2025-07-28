1fa6e7e96875842e644d4a62c8b0178ebe7b1c8e
Ellevern <elisavandk@gmail.com>
2025-07-28 15:47:32 -0400

    Started laying out basic rendering capability.

---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+unsigned int createShaderProgram(const char* vertexPath, const char* fragmentPath);
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Simple Rubrix", nullptr, nullptr);
+
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	unsigned int colorShader = createShaderProgram("colorShader.vert", "colorShader.frag");
+
+	float cubeVertices[] = {
+		-1.0f,  1.0f,  0.0f,
+		-1.0f, -1.0f,  0.0f,
+		 1.0f, -1.0f,  0.0f,
+		-1.0f,  1.0f,  0.0f,
+		-1.0f, -1.0f,  0.0f,
+		 1.0f,  1.0f,  0.0f
+	};
+
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.576f, 0.922f, 0.878f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(colorShader);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(colorShader);
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Create shader program and return its ID
+unsigned int createShaderProgram(const char* vertexPath, const char* fragmentPath)
+{
+	return 1;
+}
